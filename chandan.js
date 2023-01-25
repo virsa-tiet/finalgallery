ae1da02e1e6b1047d6025a0f5fb8361aaf5ae905
Chandankawatra123 <100226305+Chandankawatra123@users.noreply.github.com>
2023-01-25 22:22:59 +0530

    Add files via upload

---
+++ chandan.js
@@ -0,0 +1,31 @@
+$(window).load(function(){
+
+  var body = $("body"),
+      universe = $("#universe"),
+      solarsys = $("#solar-system");
+
+  var init = function() {
+    body.removeClass('view-2D opening').addClass("view-3D").delay(2000).queue(function() {
+      $(this).removeClass('hide-UI').addClass("set-speed");
+      $(this).dequeue();
+    });
+  };
+
+  var setView = function(view) { universe.removeClass().addClass(view); };
+
+
+
+  $("#data a").click(function(e) {
+    var ref = $(this).attr("class");
+    solarsys.removeClass().addClass(ref);
+    $(this).parent().find('a').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+  });
+
+  $(".set-view").click(function() { body.toggleClass("view-3D view-2D"); });
+
+
+  init();
+
+});
